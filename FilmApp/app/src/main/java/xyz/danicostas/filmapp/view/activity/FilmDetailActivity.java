d27ea12de1966abb4bc5ac3a8f4cf079a59cd2b8
Dani Costas <daniel.costas.rodriguez@bootcamp-upgrade.com>
2025-02-15 18:04:20 +0100

    Add FilmDetailActivity

---
+++ FilmApp/app/src/main/java/xyz/danicostas/filmapp/view/activity/FilmDetailActivity.java
@@ -0,0 +1,16 @@
+package xyz.danicostas.filmapp.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import xyz.danicostas.filmapp.R;
+
+public class FilmDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_detail);
+    }
+}
